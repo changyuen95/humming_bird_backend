7cd41d3684e9ec7b6aa999ba75b6b08cf3e2e5f6
alvescy95 <changyuen95@gmail.com>
2024-12-13 11:17:45 +0800

    update

--- resources/views/tour_detail.blade.php
+++ resources/views/tour_detail.blade.php
@@ -19,14 +19,11 @@
                     </div>
                 @endif
 
-                    @if(session('error'))
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        {{ session('error') }}
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    @endif
+                @if (session('error'))
+                <div class="alert alert-danger">
+                    {{ session('error') }}
+                </div>
+            @endif
 
                 <div class="card mt-3">
                     <div class="card-header">
